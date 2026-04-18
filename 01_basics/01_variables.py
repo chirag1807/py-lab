3ca6bef51bafb76710144f4ba1ca596d16483095
Chirag <chiragmakwana1807@gmail.com>
2026-04-18 23:35:23 +0530

    learn about basic variables and print statement

---
+++ 01_basics/01_variables.py
@@ -0,0 +1,12 @@
+# The Basics
+
+# 1. Hello World
+print("Hello, World!")
+
+# 2. Variables
+my_name = "Chirag"
+my_age = 23
+is_learning = True
+
+print(f"My name is {my_name}, I am {my_age} years old.")
+print(f"Is learning python? {is_learning}")
